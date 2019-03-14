09fe36bd8dde03cbf35fc2ffb3662ec43773a1dd
pheberaicheljohn <pheberaichaljohn@gmail.com>
2019-03-14 15:51:03 +0530

    toolbar setup for user

--- app/src/main/java/com/example/emedcom/AccountActivity.java
+++ app/src/main/java/com/example/emedcom/AccountActivity.java
@@ -2,12 +2,38 @@ package com.example.emedcom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class AccountActivity extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
