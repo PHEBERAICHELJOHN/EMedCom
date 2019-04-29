faf5825722592f2777c5a23e61921f225ec1073a
pheberaicheljohn <pheberaichaljohn@gmail.com>
2019-04-29 22:33:58 +0530

    main, dashboard, login

---
+++ EMedCom_project/app/src/main/java/com/example/emedcom_project/DashboardActivity.java
@@ -0,0 +1,31 @@
+package com.example.emedcom_project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        login = (Button) findViewById(R.id.buttonlogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),"Please Log In with your email and password",Toast.LENGTH_SHORT).show();
+                Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+    }
+}
