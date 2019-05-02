681c12dbc6e214ddb8fb0d8e09520316f0d71b5b
pheberaicheljohn <pheberaichaljohn@gmail.com>
2019-05-02 22:32:48 +0530

    added password forget

--- EMedCom_project/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ EMedCom_project/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -65,12 +65,12 @@ public final class R {
         public static final int adjust_height = 0x7f08001c;
         public static final int adjust_width = 0x7f08001d;
         public static final int auto = 0x7f080022;
-        public static final int dark = 0x7f08003f;
-        public static final int icon_only = 0x7f080065;
-        public static final int light = 0x7f080070;
-        public static final int none = 0x7f080081;
-        public static final int standard = 0x7f0800bd;
-        public static final int wide = 0x7f0800e4;
+        public static final int dark = 0x7f080040;
+        public static final int icon_only = 0x7f080067;
+        public static final int light = 0x7f080072;
+        public static final int none = 0x7f080083;
+        public static final int standard = 0x7f0800bf;
+        public static final int wide = 0x7f0800e6;
     }
     public static final class string {
         private string() {}
